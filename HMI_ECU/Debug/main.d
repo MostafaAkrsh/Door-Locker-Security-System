6de6dc1a522ff3fa9d87f96394f82118df7757e6
MostafaAkrsh <43042153+MostafaAkrsh@users.noreply.github.com>
2021-10-30 22:08:04 +0200

    Initial commit

---
+++ HMI_ECU/Debug/main.d
@@ -0,0 +1,5 @@
+main.o main.o: ../main.c ../timer.h ../std_types.h
+
+../timer.h:
+
+../std_types.h:
---
+++ HMI_ECU/main.c
@@ -0,0 +1,33 @@
+#include "timer.h"
+#include "avr/io.h"
+
+volatile void ledony()
+{
+	DDRC = 0xFF;
+	PORTC = ~PORTC;
+}
+
+int main()
+{
+	TIMER_Config config;
+
+	SREG |= (1<<7);
+	config.timer_id = TIMER1_ID;
+	config.timer_mode = OVERFLOW_MODE;
+	config.timer_clk = PRESCALER256;
+	config.oc_mode = DISCONNECTED;
+	config.interrupt = ON;
+	config.inital_value = 0;
+	config.compare_value = 155;
+
+	Timer_Init(&config);
+
+	 volatile void (*ptr)(void);
+	 ptr = ledony;
+
+	Timer_SetCallBack(TIMER1_ID,ptr, OVERFLOW_MODE);
+	while(1)
+	{
+
+	}
+}
